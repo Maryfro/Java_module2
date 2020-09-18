3684888f8f401e71f42fb2a188ea75c20473baad
Maryfro <memusena@gmail.com>
2020-09-18 15:34:35 +0200

    days of week project with methods for full week

--- enumProject/src/telran/application/App.java
+++ enumProject/src/telran/application/App.java
@@ -3,28 +3,23 @@ package telran.application;
 import telran.data.DaysOfWeek;
 import telran.data.Schedule;
 
-import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        DaysOfWeek monday = DaysOfWeek.MONDAY;
-        System.out.println(monday.toString().toLowerCase());
+        //DaysOfWeek monday = DaysOfWeek.MONDAY;
+     /*   System.out.println(monday.toString().toLowerCase());
         System.out.println(DaysOfWeek.FRIDAY.ordinal());
         System.out.println(Arrays.toString(DaysOfWeek.values()));
-        System.out.println(DaysOfWeek.WEDNESDAY.getTitle());
-        DaysOfWeek.THURSDAY.sayHello();
-        monday.sayHello();
-        Schedule schedule1 = new Schedule(DaysOfWeek.MONDAY);
+        System.out.println(DaysOfWeek.WEDNESDAY.getTitle());*/
+      //  DaysOfWeek.THURSDAY.sayHello();
+       // monday.sayHello();
+        DaysOfWeek[] week = {DaysOfWeek.MONDAY, DaysOfWeek.TUESDAY, DaysOfWeek.WEDNESDAY, DaysOfWeek.THURSDAY,
+                DaysOfWeek.FRIDAY, DaysOfWeek.SATURDAY, DaysOfWeek.SUNDAY};
+        Schedule schedule1 = new Schedule(week);
+        System.out.println("Schedule for one week ");
         schedule1.wakeUp();
-        Schedule schedule2 = new Schedule(DaysOfWeek.SATURDAY);
-        schedule2.wakeUp();
         System.out.println("--------------------");
-        Schedule[] schedules = {schedule1, new Schedule(DaysOfWeek.TUESDAY), new Schedule(DaysOfWeek.WEDNESDAY),
-                new Schedule(DaysOfWeek.THURSDAY), new Schedule(DaysOfWeek.FRIDAY), schedule2,
-                new Schedule(DaysOfWeek.SUNDAY)};
-        System.out.println( "Menu of the week: ");
-        for (Schedule s:schedules) {
-            s.getMenu();
-        }
+        System.out.println("Menu of the week ");
+        schedule1.getMenu();
     }
 }
--- enumProject/src/telran/data/Schedule.java
+++ enumProject/src/telran/data/Schedule.java
@@ -1,54 +1,64 @@
 package telran.data;
 
+
+import java.util.Arrays;
+
 public class Schedule {
-    private DaysOfWeek dayOfWeek;
     private DaysOfWeek[] week;
 
-    public Schedule(DaysOfWeek dayOfWeek) {
-        this.dayOfWeek = dayOfWeek;
-       // this.week = DaysOfWeek.values();
+    public Schedule(DaysOfWeek[] week) {
+        this.week = week;
     }
 
-    public DaysOfWeek getDayOfWeek() {
-        return dayOfWeek;
+
+    public DaysOfWeek[] getWeek() {
+        return week;
     }
 
-    public void setDayOfWeek(DaysOfWeek dayOfWeek) {
-        this.dayOfWeek = dayOfWeek;
+    public void setWeek(DaysOfWeek[] week) {
+        this.week = week;
     }
-    public void wakeUp(){
-        if (getDayOfWeek() == DaysOfWeek.SATURDAY || getDayOfWeek() == DaysOfWeek.SUNDAY){
-            System.out.println("sleep longer, yay");
-        }else {
-            System.out.println("wake up");
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Schedule schedule = (Schedule) o;
+        return Arrays.equals(week, schedule.week);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(week);
+    }
+
+    public void wakeUp() {
+        for (int i = 0; i < week.length; i++) {
+            if (week[i].ordinal() < 5) {
+                System.out.println((i + 1) + " wake up");
+            } else {
+                System.out.println((i+1) + " sleep longer, yay");
+            }
         }
-       // if(dayOfWeek.ordinal()>= 5)
     }
 
-    public void getMenu(){
-        switch(dayOfWeek){
-            case MONDAY:
-                System.out.println("Spaghetti");
-                break;
-            case TUESDAY:
-                System.out.println("Pizza");
-                break;
-            case WEDNESDAY:
-                System.out.println("Ice-cream");
-                break;
-            case THURSDAY:
-                System.out.println("Salad");
-                break;
-            case FRIDAY:
-                System.out.println("Soup");
-                break;
-            case SATURDAY:
-                System.out.println("Buddha Bowl");
-                break;
-            case SUNDAY:
-                System.out.println("Barbecue");
-                break;
-
+    public void getMenu() {
+        for (int i = 0; i < week.length; i++) {
+            if (week[i].equals(DaysOfWeek.MONDAY)) {
+                System.out.println(DaysOfWeek.MONDAY.getTitle() + " Spaghetti");
+            } else if (week[i].equals(DaysOfWeek.TUESDAY)) {
+                System.out.println(DaysOfWeek.TUESDAY.getTitle() + " Pizza");
+            } else if (week[i].equals(DaysOfWeek.WEDNESDAY)) {
+                System.out.println(DaysOfWeek.WEDNESDAY.getTitle() + " Ice-cream");
+            } else if (week[i].equals(DaysOfWeek.THURSDAY)) {
+                System.out.println(DaysOfWeek.THURSDAY.getTitle() + " Salad");
+            } else if (week[i].equals(DaysOfWeek.FRIDAY)) {
+                System.out.println(DaysOfWeek.FRIDAY.getTitle() + " Soup");
+            } else if (week[i].equals(DaysOfWeek.SATURDAY)) {
+                System.out.println(DaysOfWeek.SATURDAY.getTitle() + " Buddha Bowl");
+            } else {
+                System.out.println(DaysOfWeek.SUNDAY.getTitle() + " Barbecue");
+            }
         }
     }
 }
