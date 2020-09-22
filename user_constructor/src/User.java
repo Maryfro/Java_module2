acf1bb949b736580ebf68476d6ce284d0ed7f35e
Maryfro <memusena@gmail.com>
2020-09-22 10:59:14 +0200

    final update

--- user_constructor/src/User.java
+++ user_constructor/src/User.java
@@ -1,18 +1,19 @@
 public class User {
     private String name;
-    private static int id_number = 001;
+    private final int id_number;
+    private static int userNumber = 0;
 
 
     public User(String name) {
         this.name = name;
+        id_number = ++userNumber;
     }
 
     public String getName() {
         return name;
     }
 
-    public static int getId_number() {
-        id_number++;
+    public int getId_number() {
         return id_number;
     }
 
@@ -22,9 +23,11 @@ public class User {
         User user3 = new User("Kolya");
         User user4 = new User("Misha");
         System.out.println("Name: " + user1.getName() + ",  id number: " + String.format("%03d", user1.id_number));
-        System.out.println("Name: " + user2.getName() + ",  id number: " + String.format("%03d", User.getId_number()));
-        System.out.println("Name: " + user3.getName() + ",  id number: " + String.format("%03d", User.getId_number()));
-        System.out.println("Name: " + user4.getName() + ",  id number: " + String.format("%03d", User.getId_number()));
+        System.out.println("Name: " + user2.getName() + ",  id number: " + String.format("%03d", user2.getId_number()));
+        System.out.println("Name: " + user3.getName() + ",  id number: " + String.format("%03d", user3.getId_number()));
+        System.out.println("Name: " + user4.getName() + ",  id number: " + String.format("%03d", user4.getId_number()));
+
+
 
     }
 }
