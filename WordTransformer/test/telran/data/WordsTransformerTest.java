d6b6cd4404e6d4d590a0a17305ed1cc21976e029
Maryfro <memusena@gmail.com>
2020-10-08 14:51:12 +0200

    tests update

--- WordTransformer/test/telran/data/WordsTransformerTest.java
+++ WordTransformer/test/telran/data/WordsTransformerTest.java
@@ -12,24 +12,65 @@ public class WordsTransformerTest {
     WordsChecker wc3 = new ReplaceWithStars();
 
     @Test
-    public void testWordTransformNewEqualValues() {
+    public void testWordTransformNewEqualValuesWc1() {
         String output1 = WordsTransformer.WordTransformNew(input, wc1);
         Assert.assertEquals("ASD ASDF GHGFdf SDF FDG", output1);
+    }
+
+    @Test
+    public void testWordTransformNewEqualValuesWc2() {
         Assert.assertEquals("asd asdf GHGFdf sdf fdg", WordsTransformer.WordTransformNew(input, wc2));
+    }
+
+    @Test
+    public void testWordTransformNewEqualValuesWc3() {
         Assert.assertEquals("asd ASDF ****** sdf fdg", WordsTransformer.WordTransformNew(input, wc3));
     }
 
     @Test
-    public void testWordTransformNewNotNull() {
+    public void testWordTransformNewNotNullWc1() {
         Assert.assertNotNull(WordsTransformer.WordTransformNew(input, wc1));
+
+    }
+
+    @Test
+    public void testWordTransformNewNotNullWc2() {
         Assert.assertNotNull(WordsTransformer.WordTransformNew(input, wc2));
+    }
+
+    @Test
+    public void testWordTransformNewNotNullWc3() {
         Assert.assertNotNull(WordsTransformer.WordTransformNew(input, wc3));
     }
 
     @Test
-    public void testWordTransformNewNotSame() {
+    public void testWordTransformNewNotSameWc1() {
         assertNotSame(input, WordsTransformer.WordTransformNew(input, wc1));
-        assertNotSame(input, WordsTransformer.WordTransformNew(input, wc2));
-        assertNotSame(input, WordsTransformer.WordTransformNew(input, wc3));
     }
+
+    @Test
+    public void testWordTransformNewNotSameWc2() {
+        assertNotSame(input, WordsTransformer.WordTransformNew(input, wc2));
+    }
+
+    @Test
+    public void testWordTransformNewNotSameWc3() {
+        assertNotSame(input, WordsTransformer.WordTransformNew(input, wc2));
+    }
+
+    @Test
+    public void testWordTransformNewLengthOfStringWc1(){
+        assertEquals(input.length(), WordsTransformer.WordTransformNew(input, wc1).length());
+    }
+
+    @Test
+    public void testWordTransformNewLengthOfStringWc2(){
+        assertEquals(input.length(), WordsTransformer.WordTransformNew(input, wc2).length());
+    }
+
+    @Test
+    public void testWordTransformNewLengthOfStringWc3(){
+        assertEquals(input.length(), WordsTransformer.WordTransformNew(input, wc3).length());
+    }
+
 }
