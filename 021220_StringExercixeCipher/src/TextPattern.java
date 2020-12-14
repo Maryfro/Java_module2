e7152b982fe7eb45706077cc42ad30dfb7b41dd0
Maryfro <memusena@gmail.com>
2020-12-14 21:58:50 +0300

    text pattern update

--- 021220_StringExercixeCipher/src/TextPattern.java
+++ 021220_StringExercixeCipher/src/TextPattern.java
@@ -9,6 +9,8 @@ public class TextPattern {
      */
 
     boolean findPattern(String text, String pattern) {
+        StringBuilder sbText = new StringBuilder(text);
+        StringBuilder sbPattern = new StringBuilder(pattern);
         if (pattern.length() == 0) {
             return true;
         }
@@ -16,9 +18,9 @@ public class TextPattern {
             return false;
         }
         if (text.charAt(text.length() - 1) == pattern.charAt(pattern.length() - 1)) {
-            return findPattern(text.substring(0, text.length() - 1), pattern.substring(0, pattern.length() - 1));
+            return findPattern(sbText.substring(0, text.length() - 1), sbPattern.substring(0, pattern.length() - 1));
         } else {
-            return findPattern(text.substring(0, text.length() - 1), pattern);
+            return findPattern(sbText.substring(0, text.length() - 1), pattern);
         }
     }
 }
