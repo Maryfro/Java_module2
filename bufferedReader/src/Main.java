6776449e50fc3481037f1ec7f962ce7856459413
Maryfro <memusena@gmail.com>
2021-02-02 13:11:19 +0100

    buffered reader

---
+++ bufferedReader/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // List<String> fromFile = readFile("text.txt");
+        //System.out.println(fromFile);
+
+        readFromConsole();
+    }
+
+    public static List<String> readFile(String filename) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            List<String> res = new ArrayList<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                res.add(line);
+            }
+            return res;
+        }
+    }
+
+    public static void readFromConsole() throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String line;
+            while ((line = br.readLine()) != null && !line.equals("exit")) {
+                System.out.println(line);
+            }
+        }
+    }
+}
