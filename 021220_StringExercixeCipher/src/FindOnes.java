abae5801432ffddde39474f67c44b91e0c04aae4
Maryfro <memusena@gmail.com>
2021-02-17 17:05:29 +0100

    update

--- 021220_StringExercixeCipher/src/FindOnes.java
+++ 021220_StringExercixeCipher/src/FindOnes.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FindOnes {
@@ -6,13 +7,9 @@ public class FindOnes {
     // Есть диапазон чисел от a до b включительно. Найти количество чисел, содержащих цифру 1.
 
     public int findOnes(int a, int b) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = a; i <= b; i++) {
-            list.add(i);
-        }
         int count = 0;
-        for (Integer num : list) {
-            String temp = num.toString();
+        for (int i = a; i <= b; i++) {
+            String temp = String.valueOf(i);
             if (temp.contains("1"))
                 count++;
         }
