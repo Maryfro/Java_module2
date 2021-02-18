e4883b55f07b5a1ecda4122157c71c956e68b004
Maryfro <memusena@gmail.com>
2021-02-18 16:36:05 +0100

    lambdas intro tasks

---
+++ helloLambdas/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Function<List<String>, List<String>> lambda = (List<String> list) -> new ArrayList<>(new HashSet<>(list));
+
+        System.out.println(lambda.apply(new ArrayList<>(Arrays.asList("hello", "world", "cat", "hello", "cat"))));
+
+        final int a = 2, b = 3, c = 4;
+        Function<Integer, Integer> f = (x) -> a * x * x + b + x + c;
+
+        System.out.println(f.apply(1));
+    }
+
+}
