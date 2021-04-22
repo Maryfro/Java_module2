5ff928608bf55aa7000d4dbea35b8e97672ac1a9
Maryfro <memusena@gmail.com>
2021-04-22 21:08:33 +0200

    url shortener

---
+++ v1_urlShortener/src/UrlShortenerTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrlShortenerTest {
+
+    @Test
+    public void idToShortenURL_test(){
+        String res = UrlShortener.idToShortenUrl(653178733);
+        assertEquals("SmPM1", res);
+    }
+
+    @Test
+    public void shortUrlToId_test(){
+        int res = UrlShortener.shortUrlToId("booking/?23425/confirm.php");
+        assertEquals(653178733, res);
+    }
+
+}
